88a5898090c9fd1064e361f5f60a2cf4bcf3aeb8
Kacper Brzymyszkiewicz <kacper.b432121@gmail.com>
2023-04-10 13:05:19 +0200

    aa

--- src/Zadania.jsx
+++ src/Zadania.jsx
@@ -38,7 +38,7 @@ function Zadania()
 
     useEffect(() => {
         
-        fetchAPISlowa("http://localhost:5173/api/words/" + location.state)
+        fetchAPISlowa("https://fastapi-rgau-production.up.railway.app/words/" + location.state)
         
       }, []);
     
